ddc5c2b2ac31125f0f98155b5931eb5757a9b330
giorgigelashvili12 <123294128+giorgigelashvili12@users.noreply.github.com>
2024-07-04 19:06:18 +0400

    Python

---
+++ Python/03 - Multiple Assignment/main.py
@@ -0,0 +1,15 @@
+# In python you can assign as much variables as you want in one line. (declare adn assign)
+# Basic multiple assignment:
+a, b, c = 1, 2, 3
+# in this example a = 1, b = 2 and c = 3
+
+# You can also swap variable's values using 3 lines of code only
+x = 2
+y = 9
+x, y = y, x
+# In this example x = y which is 9 and y = x which is 2
+
+# One to three
+# You can create multiple variables that can hold the same value with 1 line of code
+v = b = d = 'Pizza'
+# In this example v b and d all have value 'Pizza' stored
---
+++ Python/04 - String Methods/main.py
@@ -0,0 +1,29 @@
+# Now, since you know more about strings: In python there are string methods, that let us modify the strings itself, 
+# obviously. There are different build-in string methods that you can use:
+
+some_string = 'Hello World!'
+value = 0
+
+# METHOD                    # WHAT IT DOES/DESCRIPTION            
+#-------------------------------------------------------------------------------------------------------------------                 
+some_string.capitalize()    # This method capitalizes every letter in the given string 
+#-------------------------------------------------------------------------------------------------------------------
+some_string.casefold()      # This method lowers every letter in the given string
+#-------------------------------------------------------------------------------------------------------------------
+some_string.center()        # This method returns a centered string in the terminal
+#-------------------------------------------------------------------------------------------------------------------
+some_string.count()         # This method returns how many times a letter is occured and repeated in a string
+#-------------------------------------------------------------------------------------------------------------------
+some_string.encode()        # This method returns the encoded version of the given string
+#-------------------------------------------------------------------------------------------------------------------
+some_string.endswith()      # Comprasion method, returns true if the string ends with a specified value
+#-------------------------------------------------------------------------------------------------------------------
+some_string.expandtabs()    # Expands the tab of the string
+#-------------------------------------------------------------------------------------------------------------------
+some_string.find(value)     # Finds the specified value in the string that it contains method(--value--)
+#-------------------------------------------------------------------------------------------------------------------
+some_string.format()        # Formats and returns the specified values into a string
+#-------------------------------------------------------------------------------------------------------------------
+some_string.format_map()    # Formats and returns the specified values into a string
+#-------------------------------------------------------------------------------------------------------------------
+some_string
